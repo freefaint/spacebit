a9395caf9ce6e1dc02be1bb35babd725e2e469e8
Андрей Терещенко <andrejteresenko@MacBook-Air-Andrej.local>
2021-09-02 11:43:23 +0300

    add delay

---
+++ src/fn/delay.ts
@@ -0,0 +1,33 @@
+const waiters: Array<() => void> = [];
+const timers: number[] = [];
+const DEFAULT = 1000;
+
+const place = <T>(item: T, list: T[]) => {
+  const i = list.indexOf(item);
+
+  if (i !== -1) {
+    return i;
+  }
+
+  return list.push(item) - 1;
+};
+
+export const delay = (fn: (...params: any) => void, time: number = DEFAULT, params?: any[]) => {
+  const i = place(fn, waiters);
+
+  if (timers[i]) {
+    clearTimeout(timers[i]);
+  }
+
+  timers[i] = window.setTimeout(params ? () => fn(...params) : fn, time !== undefined ? time : DEFAULT);
+};
+
+export const cancel = (fn: () => void) => {
+  const i = waiters.indexOf(fn);
+
+  if (i !== -1) {
+    clearTimeout(timers[i]);
+  }
+};
+
+const noop = () => null;
